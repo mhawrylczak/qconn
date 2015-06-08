787544e3750ab62816b470a67f6be4288395fdf6
Slawomir Trzaszczka <slawomir.trzaszczka@allegro.pl>
2015-06-08 19:12:03 +0200

    udp-server improved

--- udp-server.go
+++ udp-server.go
@@ -22,30 +22,35 @@ func toJsonMap(jsonBytes []byte) map[string]string {
         }
 	return data
 }
+
+func Listen(port string){
+	/* Lets prepare a address at any address at port 10001*/
+    	ServerAddr,err := net.ResolveUDPAddr("udp",":10001")
+    	CheckError(err)
+    
+    	/* Now listen at selected port */
+    	ServerConn, err := net.ListenUDP("udp", ServerAddr)
+    	CheckError(err) 
+    	defer ServerConn.Close()
+    
+    	buf := make([]byte, 1024)
+
+    	for {
+        	n,_,err := ServerConn.ReadFromUDP(buf)
+        	if err != nil {
+            		fmt.Println("Error: ",err)
+        	}
+        	// string to map
+        	var jsonMap = toJsonMap(buf[0:n])
+        	// end
+        	fmt.Println("Json is: ", jsonMap)
+        	if err != nil {
+            		fmt.Println("Error: ",err)
+        	}   
+    	}
+}
  
 func main() {
-    /* Lets prepare a address at any address at port 10001*/   
-    ServerAddr,err := net.ResolveUDPAddr("udp",":10001")
-    CheckError(err)
- 
-    /* Now listen at selected port */
-    ServerConn, err := net.ListenUDP("udp", ServerAddr)
-    CheckError(err)
-    defer ServerConn.Close()
- 
-    buf := make([]byte, 1024)
- 
-    for {
-        n,_,err := ServerConn.ReadFromUDP(buf)
-	if err != nil {
-            fmt.Println("Error: ",err)
-        } 
-	// string to map
-	var jsonMap = toJsonMap(buf[0:n])
-	// end
- 	fmt.Println("Json is: ", jsonMap) 
-        if err != nil {
-            fmt.Println("Error: ",err)
-        } 
-    }
+	port := ":10001"
+	Listen(port)
 }
